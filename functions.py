bd5f6800581c59bddf0a356eee49ad03fd3289d0
Yl-lY <thalyscaetano520@gmail.com>
2025-03-01 04:02:48 -0300

    correção de arquivos

--- functions.py
+++ functions.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 import requests
-from database import connect_db
 import os
 
 load_dotenv()
@@ -26,52 +25,7 @@ def collect_user_id(user_name):
 
 def collect_user_friends(user_id):
     response = requests.get(f"https://www.duolingo.com/2017-06-30/friends/users/{user_id}/followers?pageSize=500&viewerId={user_id}&_={os.getenv("TLS_TOKEN")}", headers=headers).json()
-    return response
-
-def include_competitor_db(user_name, language):
-    
-    data = collect_user_data(user_name)
-
-    if data == None:
-        print(f'Usuário {user_name} não encontrado')
-        return False
-
-    display_name = data['fullname']
-    avatar_url = 'https:' + data['avatar'] + '/xlarge'
-    xp = '0'
-
-    for i in data['languages']:
-        if i['learning'] == False:
-            continue
-        if i['language_string'] == language:
-            xp = i['points']
-            break
-
-    conn = connect_db()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-        INSERT INTO competitors (username, avatar_url, display_name, language, xp)
-        VALUES (%s, %s, %s, %s, %s)
-        ON CONFLICT (username) DO UPDATE SET xp = EXCLUDED.xp;
-    """, (user_name, avatar_url, display_name, language, xp))
-
-    conn.commit()
-    conn.close()
-
-    print(f'Usuário {user_name} salvo com sucesso')
-    return True
-
-def get_competitors_db():
-    conn = connect_db()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT username, avatar_url, display_name, language, xp FROM competitors ORDER BY xp DESC")
-    competitors = cursor.fetchall()
-
-    conn.close()
-    return competitors
-    
+    return response 
 
 def include_competitor_local():
     ...
